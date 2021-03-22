113be01da8ccbe34a4096c814beda6a0baa477f0
unknown <arvindprince10@gmail.com>
2021-03-23 02:36:52 +0530

    about me

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import './App.css';
+import React from "react";
 //components
 import MyNavbar from './components/my-navbar/mynavbar.component';
-import Mycarousal from './components/my-carousal/my-carousal.component';
+import MyCarousal from './components/my-carousal/my-carousal.component';
 import TitleMessage from './components/title-message/title-message.component';
+import About from './pages/about/about.component';
+import Container from "react-bootstrap/Container";
+import Fade from "react-reveal/Fade";
+import { Parallax } from "react-parallax";
+import './App.css';
+import image from './assets/img/parallex/background.webp'
+
+
 
 const App = () => {
-  
   return (
-    <div className="App">
+    <div className="App" style={{ position: "relative" }}>
      <MyNavbar/>
-     <Mycarousal/>
+     <MyCarousal/>
      <TitleMessage/>
+     <div>
+     <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={image}
+          bgImageAlt="parallax image"
+          strength={-200}
+        >
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+        </Parallax>
+      </div>
+     
     </div>
   );
 }
---
+++ src/pages/about/about.component.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./about.styles.css";
+import Profile from "../../assets/img/profile/profile.webp";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+
+const About = () => {
+  return (
+    <div id="about">
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mb-2 mr-2 ">
+                <Image className="profile justify-content-end" alt="profile" src={Profile} thumbnail fluid />
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className=" align-items-start p-2 my-details rounded">
+                Hi there! I am <strong>&nbsp;Aravind Maddala</strong>
+                <br />
+                I am a Full Stack Web Developer with React.js, Redux, Express.js, Node.js, and MongoDB as my tech stack.
+                <br />
+                <br />
+                In 2015, I successfully completed my Engineering with specialization in 'Computer Science'.
+                <br />
+                <br />
+                I hold experience working as 'Process Developer' in Genpact for client Google.
+                <br />
+                <br />
+                After discovering my passion for web development, I couldn’t get enough. I made websites for fun. I’m looking forward to bringing that passion to a full-time role
+                <br />
+                <br />
+                 <br />
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        Let's talk
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://github.com/Arvind-Maddala?tab=repositories" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-success">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://github.com/Arvind-Maddala?tab=repositories" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://www.linkedin.com/in/aravind-maddala-8212b7131/" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-info">
+                        LinkedIn
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default About;
