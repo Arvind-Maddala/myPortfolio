8a46d13aee133a64d8f143c762b70033b005512a
unknown <arvindprince10@gmail.com>
2021-03-23 01:23:55 +0530

    scroll

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 //components
-import MyNavbar from './components/my-navbar/mynavbar.component'
+import MyNavbar from './components/my-navbar/mynavbar.component';
+import Mycarousal from './components/my-carousal/my-carousal.component';
+import TitleMessage from './components/title-message/title-message.component';
 
 const App = () => {
   
   return (
     <div className="App">
      <MyNavbar/>
+     <Mycarousal/>
+     <TitleMessage/>
     </div>
   );
 }
